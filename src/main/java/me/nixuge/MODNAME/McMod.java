c55262df216fb0bb1d383192def70f3fb2a00d81
Nixuge <mail@nixuge.me>
2023-06-25 21:34:01 +0200

    first commit

---
+++ src/main/java/me/nixuge/MODNAME/McMod.java
@@ -0,0 +1,24 @@
+package me.nixuge.MODNAME;
+
+import lombok.Getter;
+import lombok.Setter;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(
+        modid = McMod.MOD_ID,
+        name = McMod.NAME,
+        version = McMod.VERSION,
+        clientSideOnly = true
+)
+
+@Setter
+public class McMod {
+    public static final String MOD_ID = "<MODNAME>";
+    public static final String NAME = "<FULLMODNAME>";
+    public static final String VERSION = "<MODVERSION>";
+
+
+    @Getter
+    @Mod.Instance(value = McMod.MOD_ID)
+    private static McMod instance;
+}
